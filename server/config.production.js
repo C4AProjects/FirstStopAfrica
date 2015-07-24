d8305ce800e30589341693ce5e69e95e6869f305
Yazid ABDOU WABI <yazid.wabi@gmail.com>
2015-07-24 17:00:25 +0000

    add admin page for subscriptions#2

--- server/config.production.js
+++ server/config.production.js
@@ -1,7 +1,6 @@
-var p = require('../package.json');
-var version = p.version.split('.').shift();
+
 module.exports = {
-  restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
+  restApiRoot: '/api',
   host: process.env.HOST || 'firstop.c4asolution.com',
   port: process.env.NODE_PORT || 3000,
   cors: true
